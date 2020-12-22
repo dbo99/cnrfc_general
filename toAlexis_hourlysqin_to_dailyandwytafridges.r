9b8f7c94e077d9100cfc54a12b3c89eb8f16026c
dbo99 <oconnor.david.b@gmail.com>
2020-12-22 11:50:11 -0800

    Add files via upload

--- toAlexis_hourlysqin_to_dailyandwytafridges.r
+++ toAlexis_hourlysqin_to_dailyandwytafridges.r
@@ -5,7 +5,7 @@
 
 
 
-{
+#{
 rm(list = ls())
 rstudioapi::getActiveDocumentContext
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -25,8 +25,8 @@ as_tibble(tib) #should be 143.8 not 43.8
 #                    header = T )#,
 #                   #na.strings ="",
                   # stringsAsFactors= F)
-Sys.timezone(location = TRUE)
-hours <- seq(from=as.POSIXct("1979-10-01 01:00"), to=as.POSIXct("2019-09-01 00:00"), 
+#Sys.timezone(location = TRUE)
+hours <- seq(from=as.POSIXct("1979-10-01 01:00"), to=as.POSIXct("2019-10-01 00:00"), 
              by="hour", tz = "America/Los_Angeles",
              format = "%d-%m-%Y %H:%M:%S")  
 
@@ -43,8 +43,6 @@ df <- df %>% transmute(tstep = hours,  cfs_chvc1 = 35.3147*cms_chvc1,
                        cfs_hetc1 = 35.3147*cms_hetc1 )
 
 as_tibble(df)
-tib <- df[23377,]
-as_tibble(tib) #should be 143.8 not 43.8
 maxhet <- max(df$cfs_hetc1)
 maxlnr <- max(df$cfs_lnrc1)
 maxchv <- max(df$cfs_chvc1)
@@ -52,7 +50,7 @@ maxchv <- max(df$cfs_chvc1)
 df <- df %>% mutate(cfs_hetc1 = round(cfs_hetc1, 4),
                     cfs_lnrc1 = round(cfs_lnrc1, 4),
                     cfs_chvc1 = round(cfs_chvc1, 4))
-}
+#}
 
 write.csv(df, "hetc1_chvc1_lnrc1_1hrsqin_Oct1979_Aug2019.csv")
 
@@ -76,7 +74,7 @@ as_tibble(tib) #should be 143.8 not 43.8
 #                   #na.strings ="",
 # stringsAsFactors= F)
 
-hours <- seq(from=as.POSIXct("1979-10-01 01:00"), to=as.POSIXct("2019-09-01 00:00"), 
+hours <- seq(from=as.POSIXct("1979-10-01 01:00"), to=as.POSIXct("2019-10-01 00:00"), 
              by="hour", tz = "America/Los_Angeles",
              format = "%d-%m-%Y %H:%M:%S")
 
@@ -100,7 +98,7 @@ chvc1dly <- right_join(chvc1dly, dowy, join_by = c("wy", "yday"))
 #lnrc1dly <- lnrc1 %>% group_by(year, month, day) %>% summarize(cfs = 35.3147*mean(cms), reservoir = "lnrc1")
 #as_tibble(lnrc1dly)
 
-hours <- seq(from=as.POSIXct("1979-10-01 01:00"), to=as.POSIXct("2019-09-01 00:00"), 
+hours <- seq(from=as.POSIXct("1979-10-01 01:00"), to=as.POSIXct("2019-10-01 00:00"), 
              by="hour", tz = "America/Los_Angeles",
              format = "%d-%m-%Y %H:%M:%S")
 
@@ -125,7 +123,7 @@ lnrc1dly <- right_join(lnrc1dly, dowy, join_by = c("wy", "yday"))
 #hetc1dly <- hetc1 %>% group_by(year, month, day) %>% summarize(cfs = 35.3147*mean(cms), reservoir = "hetc1")
 #as_tibble(hetc1dly)
 
-hours <- seq(from=as.POSIXct("1979-10-01 01:00"), to=as.POSIXct("2019-09-01 00:00"), 
+hours <- seq(from=as.POSIXct("1979-10-01 01:00"), to=as.POSIXct("2019-10-01 00:00"), 
              by="hour", tz = "America/Los_Angeles",
              format = "%d-%m-%Y %H:%M:%S")
 
@@ -183,13 +181,12 @@ ggplot(df_dly, aes(dowy, wy, height = cfs, group=as.factor(wy), fill = anntaf))+
                   alpha = 0.8) + #, 
                  #min_height = -minh) + 
   facet_wrap(~reservoir, nrow = 3 ) +
-  scale_fill_viridis(name = "taf/yr") + theme_gray() + scale_x_continuous(breaks = breaks, labels = labels,
+  scale_fill_viridis(name = "taf/wy") + theme_gray() + scale_x_continuous(breaks = breaks, labels = labels,
                                         sec.axis = dup_axis(name = NULL), expand = c(0,0)) +
-  labs(x = NULL) + labs(y="water year") +  theme(legend.key.width = unit(0.5, "in"), legend.key.height = unit(1, "in")) +
+  labs(x = NULL) + labs(y="water year (wy)") +  theme(legend.key.width = unit(0.5, "in"), legend.key.height = unit(1, "in")) +
   scale_y_continuous(sec.axis = dup_axis(name = NULL))
 #scale_fill_distiller(palette = "Spectral", name = "taf/yr", direction = -1 ) 
-ggsave("chvc1_hetc1_lnrc1_dlymeansqin_anntaf.png", width = 21, height = 11, units = "in", dpi = 300)
+ggsave("hetc1_chvc1_lnrc1_dlymeansqin_anntaf.png", width = 21, height = 11, units = "in", dpi = 300)
 
 
 
-labels = c("")
